643242bd04c0d6f1d5313ec534584e993f2d9c97
wanghm92 <hongmin_wang@ucsb.edu>
2019-01-20 22:42:36 -0800

    add wikibio configurations

--- configurations.py
+++ configurations.py
@@ -10,19 +10,25 @@ class Config(object):
     directions = 2
     max_grad_norm = 10
     max_len = 100
+    enc_state_mlp = True
+    unk_gen = False
 
 class ConfigWikibio(object):
-    cell = "GRU"
-    emsize = 256
+    cell = "LSTM"
+    emsize = 400
+    fdsize = 50
+    hdsize = 500
     pemsize = 5
     nlayers = 1
-    lr = 0.001
+    lr = 0.0005
     epochs = 60
     batch_size = 32
     dropout = 0
-    directions = 2
-    max_grad_norm = 10
+    directions = 1
+    max_grad_norm = 5
     max_len = 100
+    enc_state_mlp = False
+    unk_gen = True  # allow generating UNK
 
 class ConfigSmall(object):
     cell = "GRU"
@@ -36,6 +42,7 @@ class ConfigSmall(object):
     directions = 2
     max_grad_norm = 10
     max_len = 100
+    unk_gen = False
 
 class ConfigTest(object):
     cell = "GRU"
@@ -50,3 +57,4 @@ class ConfigTest(object):
     max_grad_norm = 1
     testmode = True
     max_len = 50
+    unk_gen = False
